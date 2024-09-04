e8bc0db9fce37e6f54be3d3631dab2ddfaa1b151
ALLISON PRYOR <ARP21C@FSU.EDU>
2024-09-03 22:06:44 -0400

    added grass

--- sketch.js
+++ sketch.js
@@ -5,7 +5,14 @@ function setup() {
 
 function draw() {
   background(125, 206, 235);
-
 //sun in top right corner
+fill( "yellow" ) ; 
+stroke( "orange" ) ;
+strokeWeight ( 20 ) ;
 circle(550, 50, 100) ;
+//grass on bottom half
+stroke ( 0 ) ; //black outline
+strokeWeight ( 1 ) ; //outline thickness
+fill ( "green" ) ;
+rect ( 0, 200, 600, 200 ) ;
 }
