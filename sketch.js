e5034fed670028658f06f2b7e2c5563694f0314b
ALLISON PRYOR <ARP21C@FSU.EDU>
2024-09-03 21:57:41 -0400

    Added a circle shape

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(600, 400);
+}//creates a canvas 400 pixels high and 600 pixels wide;
+
 
 function draw() {
-  background(125);
+  background(125, 206, 235);
+
+//sun in top right corner
+circle(550, 50, 100) ;
 }
